75dcb89e93c7cf12a3fb5e114e42098fc8f3731a
likeyeon <chy98csy00@naver.com>
2023-09-25 00:50:50 +0900

    feat: 게시글 삭제 Delete 기능 개발

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,8 +1,32 @@
-import React from "react";
+import { useCallback } from "react";
 import "../styles/detail.scss";
+import { useNavigate } from "react-router-dom";
 import notFound from "../assets/not-found.jpg";
+import axios from "axios";
 
-const PostDetail = ({ id, title, content, updated_at, member_id }) => {
+const PostDetail = ({
+  id,
+  title,
+  content,
+  created_at,
+  updated_at,
+  member_id,
+}) => {
+  const navigate = useNavigate();
+
+  const moveToUpdate = () => {
+    navigate(`/update/${id}`);
+  };
+
+  const deletePost = useCallback(async () => {
+    if (window.confirm("게시글을 삭제하시겠습니까?")) {
+      await axios.delete(`http://localhost:8080/posts/${id}`).then((res) => {
+        alert("삭제되었습니다.");
+        navigate("/posts");
+      });
+    }
+  }, [id, navigate]);
+
   return (
     <div className="post-detail">
       <div className="post-detail__top">
@@ -11,6 +35,10 @@ const PostDetail = ({ id, title, content, updated_at, member_id }) => {
           <span className="post-detail__updated">{updated_at}</span>
           <span> | </span>
           <span className="post-detail__memeber">{member_id}</span>
+        </div>
+        <div className="post-detail__link">
+          <button onClick={moveToUpdate}>수정</button>
+          <button onClick={deletePost}>삭제</button>
         </div>
       </div>
       <div className="post-detail__bottom">
